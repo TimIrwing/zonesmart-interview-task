845632323079b7a41669c12fff1c15d3613e35c2
Tim <timirwing@gmail.com>
2019-11-15 15:34:30 +0300

    add Container

---
+++ src/components/Container.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Container',
+};
+</script>
+
+<style scoped>
+  .container {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-image: linear-gradient(-30deg, #3b41c5 0%, #a981bb 49%, #ffc8a9 100%);
+  }
+
+</style>
