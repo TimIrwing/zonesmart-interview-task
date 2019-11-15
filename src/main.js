8fa542126f0798c04bdf8108c293787ffc1d1323
Tim <timirwing@gmail.com>
2019-11-15 18:21:08 +0300

    add vue-material to the project

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue';
+import { MdButton } from 'vue-material/dist/components';
 import Login from '@/views/Login.vue';
 import NotFound from '@/views/NotFound.vue';
+
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default.css';
+
+Vue.use(MdButton);
 
 const routes = {
   '/': Login,
