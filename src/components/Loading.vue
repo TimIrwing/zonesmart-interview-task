a6f2524f2581d8c96530edf8d8acb8a97aa17e8b
Tim <timirwing@gmail.com>
2019-11-15 19:48:29 +0300

    add Loading

---
+++ src/components/Loading.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="loading-container">
+    <div class="loading">
+      <md-progress-spinner :md-diameter="100" :md-stroke="5" md-mode="indeterminate"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Loading',
+};
+</script>
+
+<style scoped>
+  .loading-container {
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    z-index: 1000;
+    opacity: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(0, 0, 0, 0.4);
+  }
+
+  .loading {
+    background-color: #fefefe;
+    display: flex;
+    padding: .5em;
+    border-radius: 100%;
+  }
+</style>
