b9669b361e599fcc940fdc8aeb025493e2df6261
Tim <timirwing@gmail.com>
2019-11-18 11:10:06 +0300

    add category api

--- src/APIService.js
+++ src/APIService.js
@@ -2,7 +2,9 @@ const CREATE_PATH = 'http://utss.su/api/auth/jwt/create/';
 const VERIFY_PATH = 'http://utss.su/api/auth/jwt/verify/';
 const REFRESH_PATH = 'http://utss.su/api/auth/jwt/refresh/';
 const CHANNELS_PATH = 'http://utss.su/api/user_channel/';
+const CATEGORY_PATH = 'http://utss.su/api/ebay/product/category/';
 const STORAGE = window.localStorage;
+const TOKEN_INVALID = 'token_not_valid';
 const ACCESS_NAME = 'access';
 const REFRESH_NAME = 'refresh';
 
@@ -16,6 +18,67 @@ async function getJSON(url, options) {
   const response = await fetch(url, { ...defOptions, ...options });
 
   return response.json();
+}
+
+async function refreshToken() {
+  const token = STORAGE.getItem(ACCESS_NAME);
+
+  if (!token) throw new Error('No token available');
+
+  const { code } = await getJSON(VERIFY_PATH, {
+    body: JSON.stringify({ token }),
+  });
+
+  if (code === TOKEN_INVALID) {
+    const refresh = STORAGE.getItem(REFRESH_NAME);
+
+    const { access } = await getJSON(REFRESH_PATH, {
+      body: JSON.stringify({ refresh }),
+    });
+
+    if (access) {
+      STORAGE.setItem(ACCESS_NAME, access);
+    } else {
+      throw new Error('Refresh failed');
+    }
+  }
+}
+
+function encodeParams(obj) {
+  return Object.entries(obj).map(
+    (pair) => pair.map(encodeURIComponent).join('='),
+  ).join('&');
+}
+
+async function getListArray(options) {
+  const STEP = 100;
+  const params = {
+    limit: STEP,
+    offset: 0,
+    ...options,
+  };
+
+  const result = [];
+  let response;
+  do {
+    //  eslint-disable-next-line no-await-in-loop
+    response = await getJSON(`${CATEGORY_PATH}?${encodeParams(params)}`, {
+      method: 'GET',
+      headers: {
+        Authorization: `JWT ${STORAGE.getItem(ACCESS_NAME)}`,
+      },
+    });
+
+    if (response.code === TOKEN_INVALID) {
+      //  eslint-disable-next-line no-await-in-loop
+      await refreshToken();
+    } else {
+      result.push(...response.results);
+      params.offset += STEP;
+    }
+  } while (response.count > params.offset);
+
+  return result;
 }
 
 export async function setToken(email, password) {
@@ -32,27 +95,6 @@ export async function setToken(email, password) {
   STORAGE.setItem(REFRESH_NAME, refresh);
 }
 
-export async function refreshToken() {
-  const token = STORAGE.getItem(ACCESS_NAME);
-
-  if (!token) throw new Error('No token available');
-
-  const { code } = await getJSON(VERIFY_PATH, {
-    body: JSON.stringify({ token }),
-  });
-
-  if (code === 'token_not_valid') {
-    const refresh = STORAGE.getItem(REFRESH_NAME);
-
-    const { access } = await getJSON(REFRESH_PATH, {
-      body: JSON.stringify({ refresh }),
-    });
-
-    STORAGE.setItem(ACCESS_NAME, access);
-  }
-}
-
-
 export async function getChannels() {
   await refreshToken();
 
@@ -64,4 +106,18 @@ export async function getChannels() {
   });
 
   return res.results;
+}
+
+export async function getCategoryList(options) {
+  const params = {
+    ...{
+      level: 1,
+      parent_id: '',
+      is_leaf: '',
+      variations_supported: '',
+    },
+    ...options,
+  };
+
+  return getListArray(params);
 }
